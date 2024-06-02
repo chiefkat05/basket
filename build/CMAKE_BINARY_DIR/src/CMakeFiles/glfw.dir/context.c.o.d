107660e8b83cb782d2a07e97e17fbeb3eb72554e
Chiefkat05 <jamesville05@gmail.com>
2024-06-02 04:52:12 -0500

    e

--- build/CMAKE_BINARY_DIR/src/CMakeFiles/glfw.dir/context.c.o.d
+++ build/CMAKE_BINARY_DIR/src/CMakeFiles/glfw.dir/context.c.o.d
@@ -1,24 +1,19 @@
 CMAKE_BINARY_DIR/src/CMakeFiles/glfw.dir/context.c.o: \
  /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/context.c \
+ /usr/include/stdc-predef.h \
  /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/internal.h \
  /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/../include/GLFW/glfw3.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stddef.h \
- /usr/x86_64-w64-mingw32/include/stddef.h \
- /usr/x86_64-w64-mingw32/include/crtdefs.h \
- /usr/x86_64-w64-mingw32/include/corecrt.h \
- /usr/x86_64-w64-mingw32/include/_mingw.h \
- /usr/x86_64-w64-mingw32/include/_mingw_mac.h \
- /usr/x86_64-w64-mingw32/include/_mingw_secapi.h \
- /usr/x86_64-w64-mingw32/include/vadefs.h \
- /usr/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h /usr/include/stdint.h \
- /usr/include/bits/libc-header-start.h /usr/include/features.h \
- /usr/include/features-time64.h /usr/include/bits/wordsize.h \
- /usr/include/bits/timesize.h /usr/include/stdc-predef.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.1.1/include/stddef.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.1.1/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
  /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
- /usr/include/gnu/stubs.h /usr/include/bits/types.h \
- /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
- /usr/include/bits/wchar.h /usr/include/bits/stdint-intn.h \
- /usr/include/bits/stdint-uintn.h /usr/include/bits/stdint-least.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/bits/stdint-least.h \
  /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/platform.h \
  /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/null_platform.h \
  /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/wl_platform.h \
@@ -29,9 +24,7 @@ CMAKE_BINARY_DIR/src/CMakeFiles/glfw.dir/context.c.o: \
  /usr/include/bits/fp-logb.h /usr/include/bits/fp-fast.h \
  /usr/include/bits/mathcalls-helper-functions.h \
  /usr/include/bits/mathcalls.h /usr/include/inttypes.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stdarg.h \
- /usr/x86_64-w64-mingw32/include/stdarg.h \
- /usr/x86_64-w64-mingw32/include/_mingw_stdarg.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.1.1/include/stdarg.h \
  /usr/include/wayland-version.h /usr/include/xkbcommon/xkbcommon.h \
  /usr/include/stdio.h /usr/include/bits/types/__fpos_t.h \
  /usr/include/bits/types/__mbstate_t.h \
@@ -42,7 +35,7 @@ CMAKE_BINARY_DIR/src/CMakeFiles/glfw.dir/context.c.o: \
  /usr/include/xkbcommon/xkbcommon-keysyms.h \
  /usr/include/xkbcommon/xkbcommon-compat.h \
  /usr/include/xkbcommon/xkbcommon-compose.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/stdbool.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.1.1/include/stdbool.h \
  /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/xkb_unicode.h \
  /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/posix_poll.h \
  /usr/include/poll.h /usr/include/sys/poll.h /usr/include/bits/poll.h \
@@ -89,262 +82,35 @@ CMAKE_BINARY_DIR/src/CMakeFiles/glfw.dir/context.c.o: \
  /usr/include/X11/extensions/shape.h \
  /usr/include/X11/extensions/shapeconst.h \
  /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/null_joystick.h \
- /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/win32_thread.h \
- /usr/x86_64-w64-mingw32/include/windows.h \
- /usr/x86_64-w64-mingw32/include/sdkddkver.h \
- /usr/x86_64-w64-mingw32/include/excpt.h \
- /usr/x86_64-w64-mingw32/include/windef.h \
- /usr/x86_64-w64-mingw32/include/winapifamily.h \
- /usr/x86_64-w64-mingw32/include/minwindef.h \
- /usr/x86_64-w64-mingw32/include/specstrings.h \
- /usr/x86_64-w64-mingw32/include/sal.h \
- /usr/x86_64-w64-mingw32/include/concurrencysal.h \
- /usr/x86_64-w64-mingw32/include/driverspecs.h \
- /usr/x86_64-w64-mingw32/include/winnt.h \
- /usr/x86_64-w64-mingw32/include/_mingw_unicode.h /usr/include/ctype.h \
+ /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/linux_joystick.h \
+ /usr/include/linux/input.h /usr/include/sys/time.h \
+ /usr/include/sys/ioctl.h /usr/include/bits/ioctls.h \
+ /usr/include/asm/ioctls.h /usr/include/asm-generic/ioctls.h \
+ /usr/include/linux/ioctl.h /usr/include/asm/ioctl.h \
+ /usr/include/asm-generic/ioctl.h /usr/include/bits/ioctl-types.h \
+ /usr/include/sys/ttydefaults.h /usr/include/linux/types.h \
+ /usr/include/asm/types.h /usr/include/asm-generic/types.h \
+ /usr/include/asm-generic/int-ll64.h /usr/include/asm/bitsperlong.h \
+ /usr/include/asm-generic/bitsperlong.h /usr/include/linux/posix_types.h \
+ /usr/include/linux/stddef.h /usr/include/asm/posix_types.h \
+ /usr/include/asm/posix_types_64.h /usr/include/asm-generic/posix_types.h \
+ /usr/include/linux/input-event-codes.h /usr/include/linux/limits.h \
+ /usr/include/regex.h \
+ /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/posix_thread.h \
+ /usr/include/pthread.h /usr/include/sched.h /usr/include/bits/sched.h \
+ /usr/include/bits/types/struct_sched_param.h /usr/include/bits/cpu-set.h \
+ /usr/include/time.h /usr/include/bits/time.h \
+ /usr/include/bits/types/struct_tm.h \
+ /usr/include/bits/types/struct_itimerspec.h \
  /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
- /usr/x86_64-w64-mingw32/include/apiset.h \
- /usr/x86_64-w64-mingw32/include/psdk_inc/intrin-impl.h \
- /usr/x86_64-w64-mingw32/include/basetsd.h \
- /usr/x86_64-w64-mingw32/include/guiddef.h /usr/include/string.h \
- /usr/include/strings.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/x86intrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/x86gprintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/ia32intrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/adxintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/bmiintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/bmi2intrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/cetintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/cldemoteintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/clflushoptintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/clwbintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/clzerointrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/cmpccxaddintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/enqcmdintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/fxsrintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/lzcntintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/lwpintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/movdirintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/mwaitintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/mwaitxintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/pconfigintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/popcntintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/pkuintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/prfchiintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/raointintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/rdseedintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/rtmintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/serializeintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/sgxintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/tbmintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/tsxldtrkintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/uintrintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/waitpkgintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/wbnoinvdintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/xsaveintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/xsavecintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/xsaveoptintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/xsavesintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/xtestintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/hresetintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/immintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/mmintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/xmmintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/mm_malloc.h \
- /usr/include/stdlib.h /usr/include/bits/waitflags.h \
- /usr/include/bits/waitstatus.h /usr/include/alloca.h \
- /usr/include/bits/stdlib-float.h /usr/include/errno.h \
- /usr/include/bits/errno.h /usr/include/linux/errno.h \
- /usr/include/asm/errno.h /usr/include/asm-generic/errno.h \
- /usr/include/asm-generic/errno-base.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/emmintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/pmmintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/tmmintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/smmintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/wmmintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avxintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avxvnniintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avxifmaintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avxvnniint8intrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx2intrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512fintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512erintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512pfintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512cdintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512vlintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512bwintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512dqintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512vlbwintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512vldqintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512ifmaintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512ifmavlintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512vbmiintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512vbmivlintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx5124fmapsintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx5124vnniwintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512vpopcntdqintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512vbmi2intrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512vbmi2vlintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512vnniintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512vnnivlintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512vpopcntdqvlintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512bitalgintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512vp2intersectintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512vp2intersectvlintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512fp16intrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512fp16vlintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/shaintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/fmaintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/f16cintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/gfniintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/vaesintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/vpclmulqdqintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512bf16vlintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avx512bf16intrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/avxneconvertintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/amxtileintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/amxint8intrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/amxbf16intrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/amxcomplexintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/prfchwintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/keylockerintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/amxfp16intrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/mm3dnow.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/fma4intrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/ammintrin.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/xopintrin.h \
- /usr/x86_64-w64-mingw32/include/pshpack4.h \
- /usr/x86_64-w64-mingw32/include/poppack.h \
- /usr/x86_64-w64-mingw32/include/pshpack4.h \
- /usr/x86_64-w64-mingw32/include/pshpack2.h \
- /usr/x86_64-w64-mingw32/include/poppack.h \
- /usr/x86_64-w64-mingw32/include/pshpack2.h \
- /usr/x86_64-w64-mingw32/include/pshpack8.h \
- /usr/x86_64-w64-mingw32/include/pshpack8.h \
- /usr/x86_64-w64-mingw32/include/ktmtypes.h \
- /usr/x86_64-w64-mingw32/include/winbase.h \
- /usr/x86_64-w64-mingw32/include/apisetcconv.h \
- /usr/x86_64-w64-mingw32/include/minwinbase.h \
- /usr/x86_64-w64-mingw32/include/bemapiset.h \
- /usr/x86_64-w64-mingw32/include/debugapi.h \
- /usr/x86_64-w64-mingw32/include/errhandlingapi.h \
- /usr/x86_64-w64-mingw32/include/fibersapi.h \
- /usr/x86_64-w64-mingw32/include/fileapi.h \
- /usr/x86_64-w64-mingw32/include/handleapi.h \
- /usr/x86_64-w64-mingw32/include/heapapi.h \
- /usr/x86_64-w64-mingw32/include/ioapiset.h \
- /usr/x86_64-w64-mingw32/include/interlockedapi.h \
- /usr/x86_64-w64-mingw32/include/jobapi.h \
- /usr/x86_64-w64-mingw32/include/libloaderapi.h \
- /usr/x86_64-w64-mingw32/include/memoryapi.h \
- /usr/x86_64-w64-mingw32/include/namedpipeapi.h \
- /usr/x86_64-w64-mingw32/include/namespaceapi.h \
- /usr/x86_64-w64-mingw32/include/processenv.h \
- /usr/x86_64-w64-mingw32/include/processthreadsapi.h \
- /usr/x86_64-w64-mingw32/include/processtopologyapi.h \
- /usr/x86_64-w64-mingw32/include/profileapi.h \
- /usr/x86_64-w64-mingw32/include/realtimeapiset.h \
- /usr/x86_64-w64-mingw32/include/securityappcontainer.h \
- /usr/x86_64-w64-mingw32/include/securitybaseapi.h \
- /usr/x86_64-w64-mingw32/include/synchapi.h \
- /usr/x86_64-w64-mingw32/include/sysinfoapi.h \
- /usr/x86_64-w64-mingw32/include/systemtopologyapi.h \
- /usr/x86_64-w64-mingw32/include/threadpoolapiset.h \
- /usr/x86_64-w64-mingw32/include/threadpoollegacyapiset.h \
- /usr/x86_64-w64-mingw32/include/utilapiset.h \
- /usr/x86_64-w64-mingw32/include/wow64apiset.h \
- /usr/x86_64-w64-mingw32/include/winerror.h \
- /usr/x86_64-w64-mingw32/include/fltwinerror.h \
- /usr/x86_64-w64-mingw32/include/timezoneapi.h \
- /usr/x86_64-w64-mingw32/include/wingdi.h \
- /usr/x86_64-w64-mingw32/include/pshpack1.h \
- /usr/x86_64-w64-mingw32/include/winuser.h \
- /usr/x86_64-w64-mingw32/include/tvout.h \
- /usr/x86_64-w64-mingw32/include/winnls.h \
- /usr/x86_64-w64-mingw32/include/datetimeapi.h \
- /usr/x86_64-w64-mingw32/include/stringapiset.h \
- /usr/x86_64-w64-mingw32/include/wincon.h \
- /usr/x86_64-w64-mingw32/include/winver.h \
- /usr/x86_64-w64-mingw32/include/winreg.h \
- /usr/x86_64-w64-mingw32/include/reason.h \
- /usr/x86_64-w64-mingw32/include/winnetwk.h \
- /usr/x86_64-w64-mingw32/include/wnnc.h \
- /usr/x86_64-w64-mingw32/include/virtdisk.h \
- /usr/x86_64-w64-mingw32/include/cderr.h \
- /usr/x86_64-w64-mingw32/include/dde.h \
- /usr/x86_64-w64-mingw32/include/ddeml.h \
- /usr/x86_64-w64-mingw32/include/dlgs.h \
- /usr/x86_64-w64-mingw32/include/lzexpand.h \
- /usr/x86_64-w64-mingw32/include/mmsystem.h \
- /usr/x86_64-w64-mingw32/include/mmsyscom.h \
- /usr/x86_64-w64-mingw32/include/mciapi.h \
- /usr/x86_64-w64-mingw32/include/mmiscapi.h \
- /usr/x86_64-w64-mingw32/include/mmiscapi2.h \
- /usr/x86_64-w64-mingw32/include/playsoundapi.h \
- /usr/x86_64-w64-mingw32/include/mmeapi.h \
- /usr/x86_64-w64-mingw32/include/timeapi.h \
- /usr/x86_64-w64-mingw32/include/joystickapi.h \
- /usr/x86_64-w64-mingw32/include/nb30.h \
- /usr/x86_64-w64-mingw32/include/rpc.h \
- /usr/x86_64-w64-mingw32/include/rpcdce.h \
- /usr/x86_64-w64-mingw32/include/rpcdcep.h \
- /usr/x86_64-w64-mingw32/include/rpcnsi.h \
- /usr/x86_64-w64-mingw32/include/rpcnterr.h \
- /usr/x86_64-w64-mingw32/include/rpcasync.h \
- /usr/x86_64-w64-mingw32/include/shellapi.h \
- /usr/x86_64-w64-mingw32/include/winperf.h \
- /usr/x86_64-w64-mingw32/include/winsock.h \
- /usr/x86_64-w64-mingw32/include/_timeval.h \
- /usr/x86_64-w64-mingw32/include/_bsd_types.h \
- /usr/x86_64-w64-mingw32/include/inaddr.h \
- /usr/x86_64-w64-mingw32/include/psdk_inc/_socket_types.h \
- /usr/x86_64-w64-mingw32/include/psdk_inc/_fd_types.h \
- /usr/x86_64-w64-mingw32/include/psdk_inc/_ip_types.h \
- /usr/x86_64-w64-mingw32/include/psdk_inc/_ip_mreq1.h \
- /usr/x86_64-w64-mingw32/include/psdk_inc/_wsadata.h \
- /usr/x86_64-w64-mingw32/include/psdk_inc/_xmitfile.h \
- /usr/x86_64-w64-mingw32/include/psdk_inc/_wsa_errnos.h \
- /usr/x86_64-w64-mingw32/include/wincrypt.h \
- /usr/x86_64-w64-mingw32/include/bcrypt.h \
- /usr/x86_64-w64-mingw32/include/ncrypt.h \
- /usr/x86_64-w64-mingw32/include/dpapi.h \
- /usr/x86_64-w64-mingw32/include/winefs.h \
- /usr/x86_64-w64-mingw32/include/winscard.h \
- /usr/x86_64-w64-mingw32/include/wtypes.h \
- /usr/x86_64-w64-mingw32/include/rpcndr.h \
- /usr/x86_64-w64-mingw32/include/rpcnsip.h \
- /usr/x86_64-w64-mingw32/include/rpcsal.h \
- /usr/x86_64-w64-mingw32/include/ole2.h \
- /usr/x86_64-w64-mingw32/include/objbase.h \
- /usr/x86_64-w64-mingw32/include/combaseapi.h \
- /usr/x86_64-w64-mingw32/include/wtypesbase.h \
- /usr/x86_64-w64-mingw32/include/unknwnbase.h \
- /usr/x86_64-w64-mingw32/include/objidlbase.h \
- /usr/x86_64-w64-mingw32/include/cguid.h \
- /usr/x86_64-w64-mingw32/include/objidl.h \
- /usr/x86_64-w64-mingw32/include/unknwn.h \
- /usr/x86_64-w64-mingw32/include/urlmon.h \
- /usr/x86_64-w64-mingw32/include/oleidl.h \
- /usr/x86_64-w64-mingw32/include/servprov.h \
- /usr/x86_64-w64-mingw32/include/msxml.h \
- /usr/x86_64-w64-mingw32/include/oaidl.h \
- /usr/x86_64-w64-mingw32/include/propidl.h \
- /usr/x86_64-w64-mingw32/include/oleauto.h \
- /usr/x86_64-w64-mingw32/include/winioctl.h \
- /usr/x86_64-w64-mingw32/include/winsmcrd.h \
- /usr/x86_64-w64-mingw32/include/winspool.h \
- /usr/x86_64-w64-mingw32/include/prsht.h \
- /usr/x86_64-w64-mingw32/include/commdlg.h \
- /usr/x86_64-w64-mingw32/include/stralign.h \
- /usr/x86_64-w64-mingw32/include/sec_api/stralign_s.h \
- /usr/x86_64-w64-mingw32/include/winsvc.h \
- /usr/x86_64-w64-mingw32/include/mcx.h \
- /usr/x86_64-w64-mingw32/include/imm.h \
- /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/win32_time.h \
- /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/../include/GLFW/glfw3native.h \
- /usr/include/assert.h /usr/include/limits.h \
- /usr/lib/gcc/x86_64-w64-mingw32/13.1.0/include/limits.h \
- /usr/include/bits/posix1_lim.h /usr/include/bits/local_lim.h \
- /usr/include/linux/limits.h \
+ /usr/include/bits/setjmp.h \
+ /usr/include/bits/types/struct___jmp_buf_tag.h \
  /usr/include/bits/pthread_stack_min-dynamic.h \
- /usr/include/bits/pthread_stack_min.h /usr/include/bits/posix2_lim.h
+ /usr/include/bits/pthread_stack_min.h \
+ /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/posix_time.h \
+ /home/chiefkat/Documents/CxxDeps/glfw-3.4/src/../include/GLFW/glfw3native.h \
+ /usr/include/assert.h /usr/include/string.h /usr/include/strings.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.1.1/include/limits.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.1.1/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/bits/posix2_lim.h
