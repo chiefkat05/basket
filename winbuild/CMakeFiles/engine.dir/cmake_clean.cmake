dd7e837e8b936409d51cdbf68aa53df615f4d7a8
Chiefkat05 <jamesville05@gmail.com>
2024-06-12 16:22:44 -0500

    windows

---
+++ winbuild/CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/gfx/graphics.cpp.obj"
+  "CMakeFiles/engine.dir/gfx/graphics.cpp.obj.d"
+  "CMakeFiles/engine.dir/gfx/shader.cpp.obj"
+  "CMakeFiles/engine.dir/gfx/shader.cpp.obj.d"
+  "CMakeFiles/engine.dir/logic/collision.cpp.obj"
+  "CMakeFiles/engine.dir/logic/collision.cpp.obj.d"
+  "CMakeFiles/engine.dir/logic/events.cpp.obj"
+  "CMakeFiles/engine.dir/logic/events.cpp.obj.d"
+  "CMakeFiles/engine.dir/logic/game.cpp.obj"
+  "CMakeFiles/engine.dir/logic/game.cpp.obj.d"
+  "CMakeFiles/engine.dir/main.cpp.obj"
+  "CMakeFiles/engine.dir/main.cpp.obj.d"
+  "CMakeFiles/engine.dir/multiplayer.cpp.obj"
+  "CMakeFiles/engine.dir/multiplayer.cpp.obj.d"
+  "CMakeFiles/engine.dir/nms/glad/src/gl.c.obj"
+  "CMakeFiles/engine.dir/nms/glad/src/gl.c.obj.d"
+  "engine.exe"
+  "engine.pdb"
+  "libengine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
